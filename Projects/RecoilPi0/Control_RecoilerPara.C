11e05395963b5680302440f6a429b141c0ba813e
Chris Mullen <c.mullen.2@research.gla.ac.uk>
2016-11-16 14:36:55 +0000

    Adding some analysis code

---
+++ Projects/RecoilPi0/Control_RecoilerPara.C
@@ -0,0 +1,34 @@
+{
+  //Set up the input chain to be analysed
+   TChain* tree=new TChain("Selected","datachain");
+  // tree->Add("/home/chris/Documents/PhD/GoatOut/DeuteriumTarget/ProtonMain/ProtonGood/chrisPhysicsRun/Physics_POL_PPi0_OutputA2SimProtonGood.root");
+   //tree->Add("/home/chris/Documents/PhD/GoatOut/DeuteriumTarget/NeutronMain/NeutronGood/chrisPhysicsRun/Physics_POL_PPi0_OutputA2SimNeutronGood.root");
+  // tree->Add("/scratch/chris/RealData/ChrisGoatOut/Run14920/Physics_CBTaggTAPS_14920.root");
+  
+  // tree->Add("/scratch/chris/RealData/ChrisGoatOut/Run14922/Physics_CBTaggTAPS_14922.root");
+  // tree->Add("/scratch/chris/RealData/ChrisGoatOut/Para/Physics_CBTaggTAPS_*.root");
+  // tree->Add("/w/work14/chris/Run101016/ChrisOutput/Para/Physics_CBTaggTAPS_*.root");
+  tree->Add("/w/work14/chris/Run101016/ChrisOutput/Para/Physics_CBTaggTAPS_14961.root");
+//tree->Add("/home/chris/Documents/PhD/GoatOut/DeuteriumTarget/ProtonMain/ProtonBad/chrisPhysicsRun/Physics_POL_PPi0_OutputA2SimProtonBad.root");
+   //Set the ouput directory or file (if full file name is given just 1 file is created, while if a directory it will be created and filled with files of the same name as the input 
+   //gSystem->Setenv("HSOUT",
+//		   "/home/chris/Events/Projects/RecoilPi0/HelicityNDiff/Para/2Angular10Kin.root");
+ 
+   gSystem->Setenv("HSOUT",
+		   "/home/chris/Events/Projects/RecoilPi0/HelicityNDiff/60files/DiffCut/Para/NoBinsHighStatTesting.root");
+  
+   //Configure the analysis (i.e PROOF, source compilation...)
+   TString HSANA=gSystem->Getenv("HSANA");
+   gROOT->LoadMacro(HSANA+"/HSControl.C");
+   TProof* prf=HSControl(kTRUE,kFALSE); //where the proof stuff is set, kTRUE means use PROOF
+   
+   //Use an event list from a previous analysis 
+   //If only 1 file was output the event list will be in its top directory, so just give the file name
+   //If many files were ouput the event list will be in the same directory with the name ParentEventList.root
+   //HSEntryList("/PATH TO PERVIOUS EVENTLIST/ParentEventList.root");
+
+   tree->Process("Recoiler.C++");
+
+   //Not actually sure if this is needed or not...
+   if(prf)prf->ClearData(TProof::kUnregistered | TProof::kForceClear);
+ }
