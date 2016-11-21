e5551eae599b07bb2a8021369302421eb0d3ef83
Chris Mullen <c.mullen.2@research.gla.ac.uk>
2016-11-21 15:13:49 +0000

    Updating the removal of old files

--- Projects/RecoilPi0/GeneralBGSub_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./GeneralBGSub_C.so: /mnt/share/cern_FC21/root/v5.34.28/include/cintdictversion.h /mnt/share/cern_FC21/root/v5.34.28/include/RVersion.h
-GeneralBGSub_C__ROOTBUILDVERSION= 5.34/28
--- Projects/RecoilPi0/LoopBins_C.d
+++
@@ -1,4 +0,0 @@
-
-# DO NOT DELETE
-./LoopBins_C.so: /mnt/share/cern_FC21/root/v5.34.28/include/cintdictversion.h /mnt/share/cern_FC21/root/v5.34.28/include/RVersion.h
-LoopBins_C__ROOTBUILDVERSION= 5.34/28
